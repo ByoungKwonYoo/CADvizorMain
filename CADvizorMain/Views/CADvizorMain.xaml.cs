be9166c98dd9bb67add8a20530f27299bbb3fb7e
ByoungKwonYoo <ybk0614@naver.com>
2023-05-02 13:51:40 +0900

    이미지

--- CADvizorMain/Views/CADvizorMain.xaml.cs
+++ CADvizorMain/Views/CADvizorMain.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CADvizorMain.Views
 {
@@ -23,6 +11,53 @@ namespace CADvizorMain.Views
         public CADvizorMain()
         {
             InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainWindow_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowState = WindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Shutdown();
+        }
+
+        private void TxtPass_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                //아래 조건이 성립하려면 로그인하는 계정에 대해서 정합성 체크해줘야된다.
+
+
+                //새로운 창 뛰움               
+
+                //기존창 닫음
+                //this.Close();
+            }
+
+
+        }
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
         }
     }
 }
